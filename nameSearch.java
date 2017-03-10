81e5518288e354fcbd241d31ca3f2618192e17a6
AlexAriaz <alexariaz@gmail.com>
2017-03-10 17:24:16 -0600

    nameSearch now finds only the word it is looking for, case sensitive

--- nameSearch.java
+++ nameSearch.java
@@ -1,16 +1,23 @@
+import java.util.*;
 public class nameSearch{
-public static void main(Sring[] args){
-String text="I like to move it Alex move it";
+public static void main(String[] args){
+String text="I like to move it Alex move alex Alex it Apple Alex Alex Alex Alex Alex";
 String myName="Alex";
 ArrayList<String> hits=new ArrayList<String>();
-for(int i;i<text.length();i++){
-  if(text.charAt(i)==myName.charAt(i)){
-    for(int j=0;j<myName.length();j++){
-      hits
+for(int i=0;i<text.length();i++){
+  if(text.charAt(i)==myName.charAt(0)){
+    String txet=text.substring(i,i+myName.length());
+    String tmp="";
+    if (txet.equals(myName)){
+      for(int j=0;j<myName.length();j++){
+        tmp=tmp+text.charAt(i);
+        i++;
+      }
+      hits.add(tmp);
     }
   }
 }
-
+System.out.println(hits);
 
 
 }
