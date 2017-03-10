a0575c5287c2399474bc29414eafc745e10c0d56
AlexAriaz <alexariaz@gmail.com>
2017-03-10 17:49:38 -0600

    nameSearch no longer case sensitive

--- nameSearch.java
+++ nameSearch.java
@@ -1,14 +1,14 @@
 import java.util.*;
 public class nameSearch{
 public static void main(String[] args){
-String text="I like to move it Alex move alex Alex it Apple Alex Alex Alex Alex Alex";
+String text="I like to move it Alex move alex Alex it Apple Alex Alex";
 String myName="Alex";
 ArrayList<String> hits=new ArrayList<String>();
 for(int i=0;i<text.length();i++){
-  if(text.charAt(i)==myName.charAt(0)){
+  if(Character.toLowerCase(text.charAt(i))==Character.toLowerCase(myName.charAt(0))){
     String txet=text.substring(i,i+myName.length());
     String tmp="";
-    if (txet.equals(myName)){
+    if (txet.equalsIgnoreCase(myName)){
       for(int j=0;j<myName.length();j++){
         tmp=tmp+text.charAt(i);
         i++;
