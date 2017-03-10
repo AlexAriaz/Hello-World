a37b26f65bd5b7a11ce9de4acc625759021f1e78
AlexAriaz <alexariaz@gmail.com>
2017-03-10 17:55:59 -0600

    nameSearch is now a function.(searchName)

--- nameSearch.java
+++ nameSearch.java
@@ -3,22 +3,33 @@ public class nameSearch{
 public static void main(String[] args){
 String text="I like to move it Alex move alex Alex it Apple Alex Alex";
 String myName="Alex";
+System.out.println(searchName(text, myName));
+}
+
+
+
+
+
+
+public static ArrayList<String> searchName(String context, String name){
 ArrayList<String> hits=new ArrayList<String>();
-for(int i=0;i<text.length();i++){
-  if(Character.toLowerCase(text.charAt(i))==Character.toLowerCase(myName.charAt(0))){
-    String txet=text.substring(i,i+myName.length());
+for(int i=0;i<context.length();i++){
+  if(Character.toLowerCase(context.charAt(i))==Character.toLowerCase(name.charAt(0))){
+    String txet=context.substring(i,i+name.length());
     String tmp="";
-    if (txet.equalsIgnoreCase(myName)){
-      for(int j=0;j<myName.length();j++){
-        tmp=tmp+text.charAt(i);
+    if (txet.equalsIgnoreCase(name)){
+      for(int j=0;j<name.length();j++){
+        tmp=tmp+context.charAt(i);
         i++;
       }
       hits.add(tmp);
     }
   }
 }
-System.out.println(hits);
-
-
+return hits;
 }
+
+
+
+
 }
