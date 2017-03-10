c4da64073a35f4cc80424a5ceecb1b6fbd1ab11d
AlexAriaz <alexariaz@gmail.com>
2017-03-10 17:00:27 -0600

    added namesearch

---
+++ nameSearch.java
@@ -0,0 +1,17 @@
+public class nameSearch{
+public static void main(Sring[] args){
+String text="I like to move it Alex move it";
+String myName="Alex";
+ArrayList<String> hits=new ArrayList<String>();
+for(int i;i<text.length();i++){
+  if(text.charAt(i)==myName.charAt(i)){
+    for(int j=0;j<myName.length();j++){
+      hits
+    }
+  }
+}
+
+
+
+}
+}
